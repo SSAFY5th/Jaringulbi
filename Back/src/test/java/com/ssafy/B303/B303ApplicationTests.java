2116d31cce9b7d3ead07b8f43219cf18b82ba1dc
왕영문 <wym6764@gmail.com>
2021-07-27 11:03:11 +0900

    [ADD] 백 폴더구조 생성

---
+++ Back/src/test/java/com/ssafy/B303/B303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.B303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B303ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
