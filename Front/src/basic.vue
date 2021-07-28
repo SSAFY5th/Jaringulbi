f358c22a0b6fb2fb4d627ab3c1c4f1b6decb6523
hyunwoo <320woo@naver.comgit>
2021-07-29 03:27:40 +0900

    [ADD] 프론트앤드 로그인,회원가입 페이지

---
+++ Front/src/basic.vue
@@ -0,0 +1,26 @@
+<!--전반적인 틀이 되는 컴포넌트-->
+<template>
+  <div></div>
+</template>
+<script>
+// import { mapGetters } from "vuex";
+// export default {
+//   name: "",
+//   computed: {},
+//   data() {
+//     return {};
+//   },
+//   created() {},
+//   methods: {},
+// };
+</script>
+<style></style>
+
+
+
+<div class="row">
+    <div class="col-xs-6 col-md-2"></div>
+    <div class="col-xs-6 col-md-4">로고가 들어올 공간입니당</div>
+    <div class="col-xs-6 col-md-4"></div>
+    <div class="col-xs-6 col-md-2"></div>
+  </div>
