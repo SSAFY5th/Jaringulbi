ec8d085e7a45404cd550b248134c54b41bef18fd
왕영문 <wym6764@gmail.com>
2021-08-02 17:08:34 +0900

    [REFACTOR]폴더구조 정리

--- Back/src/test/java/com/ssafy/B303/B303ApplicationTests.java
+++ Back/src/test/java/com/ssafy/B303/B303ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ssafy.B303.B303;
+package com.ssafy.B303;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class B303ApplicationTests {
 
 	@Test
 	void contextLoads() {
