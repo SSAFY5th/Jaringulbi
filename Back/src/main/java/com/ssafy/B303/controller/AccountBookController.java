6307a9d09caf27f750aca2e66cd2d1b68c55d466
왕영문 <wym6764@gmail.com>
2021-08-03 11:25:01 +0900

    [REFACTOR]핫픽스

---
+++ Back/src/main/java/com/ssafy/B303/controller/AccountBookController.java
@@ -0,0 +1,15 @@
+package com.ssafy.B303.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/accountbook")
+public class AccountBookController {
+
+
+}
