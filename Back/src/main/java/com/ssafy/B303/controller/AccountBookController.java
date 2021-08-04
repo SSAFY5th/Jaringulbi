1e7bb18bfdb07a70f8a9fcc32bb9094d71c0c5a5
왕영문 <wym6764@gmail.com>
2021-08-04 20:16:22 +0900

    [ADD]가계부 월간, 일간 정보 API-14

--- Back/src/main/java/com/ssafy/B303/controller/AccountBookController.java
+++ Back/src/main/java/com/ssafy/B303/controller/AccountBookController.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,9 +29,10 @@ public class AccountBookController {
         this.accountBookService = accountBookService;
     }
 
-    @PostMapping(value = "/add")
+    @PostMapping
     public ResponseEntity<AccountBookDto> addAccountBook(@RequestParam Map<String, String> map, Model model, HttpSession session) {
         AccountBookDto accountBookDto = new AccountBookDto(
+                0,
                 LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(map.get("date")+"T00:00:00+09:00")).atZone(ZoneId.of("Asia/Seoul"))),
                 //시분초는 일단 기본값
                 Integer.parseInt(map.get("price").toString()),
@@ -44,20 +46,73 @@ public class AccountBookController {
             e.printStackTrace();
             model.addAttribute("msg", "가계부 등록 중 문제가 발생했습니다.");
         }
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<AccountBookDto>>  monthAccountBook(@RequestParam String date, HttpSession session){
-        String month = LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(date+"T00:00:00+09:00")).atZone(ZoneId.of("Asia/Seoul")))
-                .getMonth().toString();
-        List<AccountBookDto> result = null;
+    public ResponseEntity<List<AccountBookDto>>  monthAccountBook(@RequestParam String date,Model model, HttpSession session){
+        int month = LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(date+"T00:00:00+09:00")).atZone(ZoneId.of("Asia/Seoul")))
+                .getMonthValue(); //getMonth()는 AUGUST를 리턴함..
+        List<AccountBookDto> result = new LinkedList<>();
+        int monthIncomes = 0, monthOutgoings = 0, monthSum = 0;
         try {
             result = accountBookService.selectMonth(month, 1);  //user_id 받아와서 넣어야함
+            monthIncomes = accountBookService.monthIncomes(month);
+            monthOutgoings = accountBookService.monthOutgoings(month);
         } catch (Exception e){
             e.printStackTrace();
         }
-        return new ResponseEntity<List<AccountBookDto>>(result, HttpStatus.CREATED);
+        System.out.println(monthIncomes);
+        System.out.println(monthOutgoings);
+        model.addAttribute("monthIncomes", monthIncomes);
+        model.addAttribute("monthOutgoings", monthOutgoings);
+        model.addAttribute("monthSum", monthIncomes+monthOutgoings);
+        return new ResponseEntity<List<AccountBookDto>>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "detail")
+    public ResponseEntity<List<AccountBookDto>> dayAccountBook(@RequestParam String date, HttpSession session) {
+        int month = LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(date+"T00:00:00+09:00")).atZone(ZoneId.of("Asia/Seoul")))
+                .getMonthValue();
+        int day = LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(date+"T00:00:00+09:00")).atZone(ZoneId.of("Asia/Seoul")))
+                .getDayOfMonth();
+        List<AccountBookDto> result = new LinkedList<>();
+        try {
+            result = accountBookService.selectDay(month,day, 1);  //user_id 받아와서 넣어야함
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<List<AccountBookDto>>(result, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "detail")
+    public ResponseEntity<AccountBookDto> modifyAccountBook(@RequestParam Map<String, String> map, HttpSession session){
+        AccountBookDto accountBookDto = new AccountBookDto(
+                Integer.parseInt(map.get("id").toString()),
+                LocalDateTime.from(Instant.from(DateTimeFormatter.ISO_DATE_TIME.parse(map.get("date")+"T00:00:00+09:00")).atZone(ZoneId.of("Asia/Seoul"))),
+                //시분초는 일단 기본값
+                Integer.parseInt(map.get("price").toString()),
+                Integer.parseInt(map.get("category").toString()),
+                map.get("used").toString(),
+                map.get("contents").toString(),
+                1 );    //일단은 id 1. 나중에 로그인 정보에서 id값 가져와야함
+        try {
+            accountBookService.modifyAccountBook(accountBookDto);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @DeleteMapping(value = "detail")
+    public ResponseEntity modifyAccountBook(@RequestParam int id, HttpSession session) {
+        try {
+            accountBookService.deleteAccountBook(id);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
--- Back/src/main/java/com/ssafy/B303/model/service/AccountBookServiceImpl.java
+++ Back/src/main/java/com/ssafy/B303/model/service/AccountBookServiceImpl.java
@@ -3,15 +3,19 @@ package com.ssafy.B303.model.service;
 import com.ssafy.B303.model.dto.AccountBookDto;
 import com.ssafy.B303.model.mapper.AccountBookMapper;
 import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class AccountBookServiceImpl implements AccountBookService{
+    private static final Logger logger = LoggerFactory.getLogger(AccountBookServiceImpl.class);
 
     private SqlSession sqlSession;
 
@@ -24,15 +28,39 @@ public class AccountBookServiceImpl implements AccountBookService{
     }
 
     @Override
-    public List<AccountBookDto> selectMonth(String month, int user_id) throws Exception {
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("month", month);
-        hashMap.put("user_id", user_id);
-        return sqlSession.getMapper(AccountBookMapper.class).selectMonth(hashMap);
+    public List<AccountBookDto> selectMonth(int month, int user_id) throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("month", month);
+        map.put("user_id", user_id);
+        return sqlSession.getMapper(AccountBookMapper.class).selectMonth(map);
     }
 
     @Override
-    public List<AccountBookDto> selectDay(String day, int user_id) throws Exception {
-        return null;
+    public List<AccountBookDto> selectDay(int month, int day, int user_id) throws Exception {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("month", month);
+        map.put("day", day);
+        map.put("user_id", user_id);
+        return sqlSession.getMapper(AccountBookMapper.class).selectDay(map);
+    }
+
+    @Override
+    public void deleteAccountBook(int id) throws Exception {
+        sqlSession.getMapper(AccountBookMapper.class).deleteAccountBook(id);
+    }
+
+    @Override
+    public void modifyAccountBook(AccountBookDto accountBookDto) throws Exception {
+        sqlSession.getMapper(AccountBookMapper.class).modifyAccountBook(accountBookDto);
+    }
+
+    @Override
+    public int monthIncomes(int month) throws Exception {
+        return sqlSession.getMapper(AccountBookMapper.class).monthIncomes(month);
+    }
+
+    @Override
+    public int monthOutgoings(int month) throws Exception {
+        return sqlSession.getMapper(AccountBookMapper.class).monthOutgoings(month);
     }
 }
