d6370ca202929a72a210e73c51459a569afcfee2
hyunwoo <320woo@naver.comgit>
2021-08-11 10:14:48 +0900

    [FIX] 컴포넌트명 변경

---
+++ Front/src/components/challenge/Challenge.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div><CLHeader></CLHeader></div>
+    <div>모든 챌린지</div>
+
+    <div>
+      <ChallengeList
+        v-for="(challenge, index) in challenges"
+        :key="index"
+        :image="challenge.image"
+        :contents="challenge.contents"
+      ></ChallengeList>
+    </div>
+  </div>
+</template>
+
+<script>
+import CLHeader from "@/layout/CL_Header.vue";
+import ChallengeList from "@/components/challenge/ChallengeList.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Challenge",
+  components: {
+    CLHeader,
+    ChallengeList,
+  },
+  computed: {
+    ...mapGetters(["challenges"]),
+  },
+  created() {
+    this.$store.dispatch("getChallengs");
+  },
+};
+</script>
+<style>
+</style>
