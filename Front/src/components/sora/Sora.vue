42485e350586bc492fedec85ecc29f21d88816c2
hataerin <goil1113@likelion.org>
2021-08-19 22:22:28 +0900

    [ADD] 마법의 소라고둥 대답 추가

--- Front/src/components/sora/Sora.vue
+++ Front/src/components/sora/Sora.vue
@@ -3,11 +3,14 @@
   <div>
     <Header></Header>
     <div class="main-content">
-      <div id="post-title" class="mt-5">
-        {{ this.$store.state.user.nickname }}님, 어떤 고민이 있으신가요?
+      <div id="post-title" class="mt-4">
+        <span style="color:#7a69e6;">{{ this.$store.state.user.nickname }}</span>님, 어떤 고민이 있으신가요?
         <br />소라고둥님께 물어보세요! <br />
+        <br />✨마법의 소라고둥 사용법✨<br />
       </div>
-       <span id="post-upcount"> ex) 치킨 시킬까요? 말까요? </span>
+        <span id="post-upcount"> 질문은 한번에 하나씩, </span><br>
+        <span id="post-upcount"> 완전한 문장으로 하는 것이 좋습니다 </span><br>
+       <span id="post-upcount"> ex) 취업을 할 수 있을까요?  치킨을 시킬까요? </span>
       <div class="mt-3">
         <b-img
           :src="require('@/assets/sora.jpg')"
@@ -64,35 +67,143 @@ export default {
       //     // 1초 후 sound2.mp3 일시정지
       //     audio.pause();
       //   }, 5000);
-      this.num = Math.random() * 3;
+      this.num = Math.random() * 21;
       console.log(this.num);
       if (this.num >= 0 && this.num < 1) {
-        this.text = "안돼";
+        this.text = "노력 없이는 얻는 것도 없다";
       }
       if (this.num >= 1 && this.num < 2) {
-        this.text = "돼";
+        this.text = "쉽게 생각하라";
       }
       if (this.num >= 2 && this.num < 3) {
-        this.text = "가만히 있어";
+        this.text = "다투지 마라";
+      }
+      if (this.num >= 3 && this.num < 4) {
+        this.text = "지는 것이 이기는 것이다";
+      }
+      if (this.num >= 4 && this.num < 5) {
+        this.text = "감사함을 잊지 말아라";
+      }
+      if (this.num >= 5 && this.num < 6) {
+        this.text = "남을 탓하지 마라";
+      }
+      if (this.num >= 6 && this.num < 7) {
+        this.text = "급할수록 돌아가라";
+      }
+      if (this.num >= 7 && this.num < 8) {
+        this.text = "있을 수 없는 일이다";
+      }
+      if (this.num >= 8 && this.num < 9) {
+        this.text = "단순해져라";
+      }
+      if (this.num >= 9 && this.num < 10) {
+        this.text = "부족한 것을 인정하라";
+      }
+      if (this.num >= 10 && this.num < 11) {
+        this.text = "현실 직시";
+      }
+      if (this.num >= 11 && this.num < 12) {
+        this.text = "조급해 하지 말아라";
+      }
+      if (this.num >= 12 && this.num < 13) {
+        this.text = "이 또한 지나간다";
+      }
+      if (this.num >= 13 && this.num < 14) {
+        this.text = "곧 알게 된다";
+      }
+      if (this.num >= 14 && this.num < 15) {
+        this.text = "물론이다";
+      }
+      if (this.num >= 15 && this.num < 16) {
+        this.text = "그때가 지금이다";
+      }
+      if (this.num >= 16 && this.num < 17) {
+        this.text = "때로는 우연에 기대라";
+      }
+      if (this.num >= 17 && this.num < 18) {
+        this.text = "고민이 아니다";
+      }
+      if (this.num >= 18 && this.num < 19) {
+        this.text = "신념을 지켜라";
+      }
+      if (this.num >= 19 && this.num < 20) {
+        this.text = "신중히 행동하라";
+      }
+      if (this.num >= 20 && this.num < 21) {
+        this.text = "후회할 수도 있다";
       }
     },
     replay() {
       var sound =
-        "https://docs.google.com/uc?export=open&id=1eC7sDbpWRBOIr9FBEn1UbsDsJD9tvjIg";
+        "https://jaringulbi.s3.ap-northeast-2.amazonaws.com/sora.m4a";
       console.log("replay it");
       var audio = new Audio(sound);
       audio.pause();
 
-      this.num = Math.random() * 3;
+      this.num = Math.random() * 21;
       console.log(this.num);
       if (this.num >= 0 && this.num < 1) {
-        this.text = "안돼";
+        this.text = "노력 없이는 얻는 것도 없다";
       }
       if (this.num >= 1 && this.num < 2) {
-        this.text = "돼";
+        this.text = "쉽게 생각하라";
       }
       if (this.num >= 2 && this.num < 3) {
-        this.text = "가만히 있어";
+        this.text = "다투지 마라";
+      }
+      if (this.num >= 3 && this.num < 4) {
+        this.text = "지는 것이 이기는 것이다";
+      }
+      if (this.num >= 4 && this.num < 5) {
+        this.text = "감사함을 잊지 말아라";
+      }
+      if (this.num >= 5 && this.num < 6) {
+        this.text = "남을 탓하지 마라";
+      }
+      if (this.num >= 6 && this.num < 7) {
+        this.text = "급할수록 돌아가라";
+      }
+      if (this.num >= 7 && this.num < 8) {
+        this.text = "있을 수 없는 일이다";
+      }
+      if (this.num >= 8 && this.num < 9) {
+        this.text = "단순해져라";
+      }
+      if (this.num >= 9 && this.num < 10) {
+        this.text = "부족한 것을 인정하라";
+      }
+      if (this.num >= 10 && this.num < 11) {
+        this.text = "현실 직시";
+      }
+      if (this.num >= 11 && this.num < 12) {
+        this.text = "조급해 하지 말아라";
+      }
+      if (this.num >= 12 && this.num < 13) {
+        this.text = "이 또한 지나간다";
+      }
+      if (this.num >= 13 && this.num < 14) {
+        this.text = "곧 알게 된다";
+      }
+      if (this.num >= 14 && this.num < 15) {
+        this.text = "물론이다";
+      }
+      if (this.num >= 15 && this.num < 16) {
+        this.text = "그때가 지금이다";
+      }
+      if (this.num >= 16 && this.num < 17) {
+        this.text = "때로는 우연에 기대라";
+      }
+      if (this.num >= 17 && this.num < 18) {
+        this.text = "고민이 아니다";
+      }
+      if (this.num >= 18 && this.num < 19) {
+        this.text = "신념을 지켜라";
+      }
+      if (this.num >= 19 && this.num < 20) {
+        this.text = "신중히 행동하라";
+      }
+      if (this.num >= 20 && this.num < 21) {
+        this.text = "후회할 수도 있다";
       }
     },
   },
